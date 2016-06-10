1329c8e3ca2de134ecd3123f76e54a643c8b1796
Michael Marner <michael@20papercups.net>
2016-06-10 23:15:47 +0930

    Bring authorizer's authenticate method up to current API spec.
    Code mostly based on simple-auth's devise authorizer.

--- app/authorizers/django-rest.js
+++ app/authorizers/django-rest.js
@@ -2,14 +2,10 @@ import Base from 'ember-simple-auth/authorizers/base';
 import isSecureUrl from './../utils/is-secure-url';
 
 export default Base.extend({
-  authorize: function(jqXHR, requestOptions) {
-    var secureData = this.get('session.secure');
-    var accessToken = secureData.token;
-    if (this.get('session.isAuthenticated') && !Ember.isEmpty(accessToken)) {
-      if (!isSecureUrl(requestOptions.url)) {
-        Ember.Logger.warn('Credentials are transmitted via an insecure connection - use HTTPS to keep them secure.');
-      }
-      jqXHR.setRequestHeader('Authorization', 'Token ' + accessToken);
+  authorize(data, block) {
+    let accessToken = data['token'];
+    if (!isEmpty(accessToken)) {
+      block('Authorization', 'Token ' + accessToken);
     }
   }
 });
