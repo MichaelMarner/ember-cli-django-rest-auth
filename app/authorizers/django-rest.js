7c80966bf8546022835b7bf3e46dc1fcf1b9c262
Michael Marner <michael@20papercups.net>
2016-07-22 07:18:54 +0930

    Fixes isEmpty call to be attached to Ember.

--- app/authorizers/django-rest.js
+++ app/authorizers/django-rest.js
@@ -1,10 +1,11 @@
 import Base from 'ember-simple-auth/authorizers/base';
 import isSecureUrl from './../utils/is-secure-url';
+import Ember from 'ember'
 
 export default Base.extend({
   authorize(data, block) {
     let accessToken = data['token'];
-    if (!isEmpty(accessToken)) {
+    if (!Ember.isEmpty(accessToken)) {
       block('Authorization', 'Token ' + accessToken);
     }
   }
